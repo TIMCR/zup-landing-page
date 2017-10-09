a5be7116bea541e8dd9fcf8b6e2b755331754a56
João Paulo Polles <joaopaulopolles@gmail.com>
2017-10-09 15:44:50 -0300

    atualização no prod_build

--- prod_build.sh
+++ prod_build.sh
@@ -2,57 +2,57 @@
 NODE_ENV=production
 CITY_NAME=Marechal\ Cândido\ Rondon
 PAGE_TITLE=VC\_MCR
-APPLICATION_NAME=Sistema VC_MCR
-API_URL=http://zup.mcr.pr.gov.br:8282
-WEB_APP_LINK=http://zup.mcr.pr.gov.br/web
+APPLICATION_NAME=Sistema VcMCR
+API_URL=http://192.168.1.34:8282
+WEB_APP_LINK=http://192.168.1.33/web
 
 git pull
 
-rm -rf ../bem-vindo/*
+rm -rf /var/www/html/bem-vindo/*
 
 gulp
 
-# sed -i '' "s@CITY_NAME@$CITY_NAME@g" ../index.html
+# sed -i '' "s@CITY_NAME@$CITY_NAME@g" /var/www/html/bem-vindo/index.html
 #
 # if [ -z "$PAGE_TITLE" ]; then
-#         sed -i '' "s@PAGE_TITLE@ZUP@g" ../index.html
+#         sed -i '' "s@PAGE_TITLE@ZUP@g" /var/www/html/bem-vindo/index.html
 # else
-#         sed -i '' "s@PAGE_TITLE@$PAGE_TITLE@g" ../index.html
+#         sed -i '' "s@PAGE_TITLE@$PAGE_TITLE@g" /var/www/html/bem-vindo/index.html
 # fi
 #
 # if [ -z "$APPLICATION_NAME" ]; then
-#         sed -i '' "s@APPLICATION_NAME@ZUP@g" ../index.html
+#         sed -i '' "s@APPLICATION_NAME@ZUP@g" /var/www/html/bem-vindo/index.html
 # else
-#         sed -i '' "s@APPLICATION_NAME@$APPLICATION_NAME@g" ../index.html
+#         sed -i '' "s@APPLICATION_NAME@$APPLICATION_NAME@g" /var/www/html/bem-vindo/index.html
 # fi
 #
-# sed -i '' "s@API_URL@$API_URL@g" ./dist/scripts/main.js
+# sed -i '' "s@API_URL@$API_URL@g" /var/www/html/bem-vindo/scripts/main.js
 # if [ -z "$IOS_APP_LINK" ]; then
-#         sed -i '' "s@HAS_IOS_APP_LINK@none@g" ../styles/main.css
+#         sed -i '' "s@HAS_IOS_APP_LINK@none@g" /var/www/html/bem-vindo/styles/main.css
 # else
-#         sed -i '' "s@IOS_APP_LINK@$IOS_APP_LINK@g" ../index.html
-#         sed -i '' "s@HAS_IOS_APP_LINK@block@g" ../styles/main.css
+#         sed -i '' "s@IOS_APP_LINK@$IOS_APP_LINK@g" /var/www/html/bem-vindo/index.html
+#         sed -i '' "s@HAS_IOS_APP_LINK@block@g" /var/www/html/bem-vindo/styles/main.css
 # fi
 #
 # if [ -z "$ANDROID_APP_LINK" ]; then
-#         sed -i '' "s@HAS_ANDROID_APP_LINK@none@g" ../styles/main.css
+#         sed -i '' "s@HAS_ANDROID_APP_LINK@none@g" /var/www/html/bem-vindo/styles/main.css
 # else
-#         sed -i '' "s@ANDROID_APP_LINK@$ANDROID_APP_LINK@g" ../index.html
-#         sed -i '' "s@HAS_ANDROID_APP_LINK@block@g" ../styles/main.css
+#         sed -i '' "s@ANDROID_APP_LINK@$ANDROID_APP_LINK@g" /var/www/html/bem-vindo/index.html
+#         sed -i '' "s@HAS_ANDROID_APP_LINK@block@g" /var/www/html/bem-vindo/styles/main.css
 # fi
 #
 # if [ -z "$WEB_APP_LINK" ]; then
-#         sed -i '' "s@HAS_WEB_APP_LINK@none@g" ../styles/main.css
+#         sed -i '' "s@HAS_WEB_APP_LINK@none@g" /var/www/html/bem-vindo/styles/main.css
 # else
-#         sed -i '' "s@WEB_APP_LINK@$WEB_APP_LINK@g" ../index.html
-#         sed -i '' "s@HAS_WEB_APP_LINK@block@g" ../styles/main.css
+#         sed -i '' "s@WEB_APP_LINK@$WEB_APP_LINK@g" /var/www/html/bem-vindo/index.html
+#         sed -i '' "s@HAS_WEB_APP_LINK@block@g" /var/www/html/bem-vindo/styles/main.css
 # fi
 #
 # [ -f terms.html ] && TERMS_AND_CONDITIONS_HTML=`cat terms.html`
 #
 # if [ -z "$TERMS_AND_CONDITIONS_HTML" ]; then
-#         sed -i '' "s@HAS_TERMS_AND_CONDITIONS@none@g" ../styles/main.css
+#         sed -i '' "s@HAS_TERMS_AND_CONDITIONS@none@g" /var/www/html/bem-vindo/styles/main.css
 # else
-#         perl -i -pe "s@TERMS_AND_CONDITIONS_HTML@$TERMS_AND_CONDITIONS_HTML@g" ../index.html
-#         sed -i '' "s@HAS_TERMS_AND_CONDITIONS@block@g" ../styles/main.css
+#         perl -i -pe "s@TERMS_AND_CONDITIONS_HTML@$TERMS_AND_CONDITIONS_HTML@g" /var/www/html/bem-vindo/index.html
+#         sed -i '' "s@HAS_TERMS_AND_CONDITIONS@block@g" /var/www/html/bem-vindo/styles/main.css
 # fi
